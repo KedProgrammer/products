d9347041277468bc3611bb6bc47a6af0ef2f2b93
daniel <shiriux1@gmail.com>
2018-10-09 00:04:18 -0500

    hcangtes

--- db/migrate/20181009050338_create_join_table_products_categories.rb
+++ db/migrate/20181009050338_create_join_table_products_categories.rb
@@ -1,9 +1,8 @@
 class CreateJoinTableProductsCategories < ActiveRecord::Migration[5.2]
   def change
     create_join_table :products, :categories do |t|
-      t.references :product, foreign_key: true
-      t.references :category, foreign_key: true
-
+      # t.index [:product_id, :category_id]
+      # t.index [:category_id, :product_id]
     end
   end
 end
